1a103d87cf14989cba7fb011842be49190c10411
Abel Halo <zxz054321@163.com>
2016-02-24 11:01:09 +0800

    Command Line Applications

---
+++ app/Console/MainTask.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: Abel Halo <zxz054321@163.com>
+ */
+
+
+class MainTask extends \Phalcon\CLI\Task
+{
+    public function mainAction()
+    {
+        echo "\nThis is the default task and the default action \n";
+    }
+}
